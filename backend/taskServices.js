a08f5e54aab9e5d6ec46c592da739df0bb255135
Dylan Tompkins <dltompki@outlook.com>
2024-06-01 14:43:41 -0700

    fixed some bugs, no mongodb url

--- backend/taskServices.js
+++ backend/taskServices.js
@@ -1,19 +1,19 @@
 /* eslint-disable space-before-function-paren */
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const userSchema = require('./userSchema')
-const taskSchema = require('./Task')
-dotenv.config()
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userSchema = require("./userSchema");
+const taskSchema = require("./task");
+dotenv.config();
 
-let dbConnection
+let dbConnection;
 function getDbConnection() {
   if (!dbConnection) {
     dbConnection = mongoose.createConnection(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+      useUnifiedTopology: true,
+    });
   }
-  return dbConnection
+  return dbConnection;
 }
 
 // // list all tasks from user with id
@@ -34,54 +34,55 @@ function getDbConnection() {
 // }
 
 async function getTasks(userId) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
     const tasks = await taskModel.find({ userId, deleted: false });
-    console.log('Tasks retrieved:', tasks);
+    console.log("Tasks retrieved:", tasks);
     return tasks;
   } catch (error) {
-    console.log('Error fetching tasks:', error);
+    console.log("Error fetching tasks:", error);
     return undefined;
   }
 }
 
 async function getDeletedTasks(userId) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
-      const deletedTasks = await taskModel.find({ userId: userId, deleted: true });
-      console.log('Deleted tasks retrieved for user:', deletedTasks);
-      return deletedTasks;
+    const deletedTasks = await taskModel.find({
+      userId: userId,
+      deleted: true,
+    });
+    console.log("Deleted tasks retrieved for user:", deletedTasks);
+    return deletedTasks;
   } catch (error) {
-      console.log('Error fetching deleted tasks for user:', error);
-      return [];
+    console.log("Error fetching deleted tasks for user:", error);
+    return [];
   }
 }
 
-
 async function permanentlyDeleteTask(taskID) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
     const task = await taskModel.findById(taskID);
     if (!task) {
-      console.log('No task found with ID:', taskID);
+      console.log("No task found with ID:", taskID);
       return null;
     }
     const result = await taskModel.findByIdAndDelete(taskID);
-    console.log('Task permanently deleted:', result);
+    console.log("Task permanently deleted:", result);
     return result;
   } catch (error) {
-    console.log('Error permanently deleting task:', error);
+    console.log("Error permanently deleting task:", error);
     return undefined;
   }
 }
 
-
 // get single task thru taskID
 async function getSingleTask(taskID) {
-  const taskModel = getDbConnection().model('Task', taskSchema)
-  const task = await taskModel.findOne({ _id: taskID })
-  console.log(task)
-  return task
+  const taskModel = getDbConnection().model("Task", taskSchema);
+  const task = await taskModel.findOne({ _id: taskID });
+  console.log(task);
+  return task;
 }
 
 // // delete a task by its ID
@@ -97,84 +98,92 @@ async function getSingleTask(taskID) {
 // }
 
 async function markTaskAsDeleted(taskID) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
-    const updatedTask = await taskModel.findByIdAndUpdate(taskID, { deleted: true }, { new: true });
-    console.log('Task marked as deleted');
+    const updatedTask = await taskModel.findByIdAndUpdate(
+      taskID,
+      { deleted: true },
+      { new: true },
+    );
+    console.log("Task marked as deleted");
     return updatedTask;
   } catch (error) {
-    console.log('Error marking task as deleted:', error);
+    console.log("Error marking task as deleted:", error);
     return undefined;
   }
 }
 
-
-
 // add task
 // as of now, it works by using postman to add a json of the task
 async function addTask(task) {
-  const TaskModel = getDbConnection().model('Task', taskSchema)
+  const TaskModel = getDbConnection().model("Task", taskSchema);
   try {
     // passing the JSON content of the Document:
-    const taskToAdd = new TaskModel(task)
-    const savedTask = await taskToAdd.save()
-    return savedTask
+    const taskToAdd = new TaskModel(task);
+    const savedTask = await taskToAdd.save();
+    return savedTask;
   } catch (error) {
-    console.log(error)
-    return false
+    console.log(error);
+    return false;
   }
 }
 
 // edit tasks
 async function editTask(taskId, taskEdits) {
-  const taskModel = getDbConnection().model('Task', taskSchema)
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
     const editedTask = await taskModel.findOneAndUpdate(
       { _id: taskId },
       taskEdits,
-      { returnOriginal: false }
-    )
-    return editedTask
+      { returnOriginal: false },
+    );
+    return editedTask;
   } catch (error) {
     //console.log('Failed to edit task')
-    return false
+    return false;
   }
 }
 
 async function recoverTask(taskID) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
-    const updatedTask = await taskModel.findByIdAndUpdate(taskID, { deleted: false }, { new: true });
-    console.log('Task recovered:', updatedTask);
+    const updatedTask = await taskModel.findByIdAndUpdate(
+      taskID,
+      { deleted: false },
+      { new: true },
+    );
+    console.log("Task recovered:", updatedTask);
     return updatedTask;
   } catch (error) {
-    console.log('Error recovering task:', error);
+    console.log("Error recovering task:", error);
     return null;
   }
 }
 
 // Function to delete all tasks for a user
 async function deleteAllTasks(userId) {
-  const taskModel = getDbConnection().model('Task', taskSchema);
+  const taskModel = getDbConnection().model("Task", taskSchema);
   try {
-      const result = await taskModel.deleteMany({ userId: userId, deleted: true });
-      console.log('Deleted tasks count:', result.deletedCount);
-      return result;
+    const result = await taskModel.deleteMany({
+      userId: userId,
+      deleted: true,
+    });
+    console.log("Deleted tasks count:", result.deletedCount);
+    return result;
   } catch (error) {
-      console.error('Error deleting all tasks for user:', error);
-      throw error;
+    console.error("Error deleting all tasks for user:", error);
+    throw error;
   }
 }
 
 module.exports = {
   getTasks,
-  getSingleTask, 
+  getSingleTask,
   markTaskAsDeleted,
   getDeletedTasks,
   permanentlyDeleteTask,
   addTask,
   editTask,
   recoverTask,
-  deleteAllTasks
-}
-
+  deleteAllTasks,
+};
